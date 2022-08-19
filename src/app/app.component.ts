874e583d39a0b8add93f82e5c2b0ecceb2356753
jonesprabu <jonesprabu@gmail.com>
2022-08-19 20:43:54 +0530

    Layout Module added - 2 files modified, 2 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
