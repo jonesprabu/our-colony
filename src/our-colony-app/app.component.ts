a9d6e6c7b89a45bbf07b8c769cccf7d0acb7e390
jonesprabu <jonesprabu@gmail.com>
2022-08-20 23:41:55 +0530

    1 binaries uploaded

--- src/our-colony-app/app.component.ts
+++ src/our-colony-app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'our-colony-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/our-colony-app/core/components/under-construction/under-construction.component.ts
+++ src/our-colony-app/core/components/under-construction/under-construction.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-under-construction',
   templateUrl: './under-construction.component.html',
-  styleUrls: ['./under-construction.component.css']
+  styleUrls: ['./under-construction.component.css'],
 })
 export class UnderConstructionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
